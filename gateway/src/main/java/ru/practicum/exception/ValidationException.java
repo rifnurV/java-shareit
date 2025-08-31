921048ca4ef583d8911ae50edc4c0cd27826f03c
Rifnur Vakhitov <rifnur69v@yandex.ru>
2025-08-31 23:28:24 +0300

    CheckStyle

--- gateway/src/main/java/ru/practicum/exception/ValidationException.java
+++ gateway/src/main/java/ru/practicum/exception/ValidationException.java
@@ -1,8 +1,5 @@
 package ru.practicum.exception;
 
-import lombok.Builder;
-import lombok.Getter;
-
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
